76ed8e0ead325310b58a365c81e32ef98d15ee13
David Truan <daxid@windowslive.com>
2017-03-20 12:26:17 +0100

    Done

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
@@ -1,5 +1,10 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.FilterWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 
 /**
  * 
@@ -15,6 +20,36 @@ public class Application {
   }
 
   IStreamDecoratorController getStreamDecoratorController() {
-    return null;
+    return new IStreamDecoratorController() {
+        @Override
+        public Reader decorateReader(Reader inputReader) {
+            return inputReader;
+        }
+
+        @Override
+        public Writer decorateWriter(Writer outputWriter) {
+            outputWriter = new FilterWriter(outputWriter) {
+                @Override
+                public void write(String str, int off, int len) throws IOException {
+                    write(str.toCharArray(), off, len);
+                }
+
+                @Override
+                public void write(char[] cbuf, int off, int len) throws IOException {
+                    for (int i = off; i < off + len; ++i) {
+                        write(cbuf[i]);
+                    }
+
+                }
+
+                @Override
+                public void write(int c) throws IOException {
+                    if(c != 'A' && c != 'a')
+                     out.write(Character.toUpperCase(c));
+                }
+            };
+            return outputWriter;
+        }
+    };
   }
 }
